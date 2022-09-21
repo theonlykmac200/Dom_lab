725b3f75e607ca9ff07589a6ff6bb27226ee3663
Kelly Mcdonald <kellymcdonald@Kellys-iMac-2.local>
2022-09-20 21:21:35 -0700

    task 1 complete

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const mainEl = document.querySelector("main");
 console.log(mainEl);
+
+mainEl.style.backgroundColor = "var(--main-bg)"
+
+mainEl.innerHTML = ("<h1>SEI Rocks!</h1>");
+
+mainEl.classList.add("flex-ctr");
