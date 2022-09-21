b5896c1eeda3a3bc9d570246e1870d7d871afca8
Kelly Mcdonald <kellymcdonald@Kellys-iMac-2.local>
2022-09-20 22:17:28 -0700

    finshed the lab

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
 const mainEl = document.querySelector("main");
 console.log(mainEl);
 
@@ -6,3 +14,22 @@ mainEl.style.backgroundColor = "var(--main-bg)"
 mainEl.innerHTML = ("<h1>SEI Rocks!</h1>");
 
 mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+
+topMenuEl.style.height = "100%";
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+
+topMenuEl.classList.add("flex-around")
+
+
+
+  menuLinks.forEach((item) => {
+    let newLink =document.createElement("a")
+    newLink.setAttribute("href", item.href)
+    newLink.textContent = (item.text)
+    topMenuEl.append(newLink)
+
+    console.log(newLink)
+});
